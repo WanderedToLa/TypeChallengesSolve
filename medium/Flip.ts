175e913ac8145497e55883cff9c6bb6cb2b4d58a
김우석(Kim Woo Seok) <rladntjr9808@gmail.com>
2023-10-22 19:29:07 +0900

    Update 4179 Filp

---
+++ medium/Flip.ts
@@ -0,0 +1,35 @@
+/*
+  4179 - Flip
+  -------
+  by Farhan Kathawala (@kathawala) #medium #object
+
+  ### Question
+
+  Implement the type of `just-flip-object`. Examples:
+
+  ```typescript
+  Flip<{ a: "x", b: "y", c: "z" }>; // {x: 'a', y: 'b', z: 'c'}
+  Flip<{ a: 1, b: 2, c: 3 }>; // {1: 'a', 2: 'b', 3: 'c'}
+  Flip<{ a: false, b: true }>; // {false: 'a', true: 'b'}
+  ```
+
+  No need to support nested objects and values which cannot be object keys such as arrays
+
+  > View on GitHub: https://tsch.js.org/4179
+*/
+
+import type { Equal, Expect , NotEqual } from "../utils";
+
+
+// Key = a T[Key] = 'pi' keyof T = 'a'
+type Flip<T extends Record<string , string | number | boolean>> = {
+  [Key in keyof T as `${T[Key]}`]: Key
+}
+
+type cases = [
+  Expect<Equal<{ a: 'pi' }, Flip<{ pi: 'a' }>>>,
+  Expect<NotEqual<{ b: 'pi' }, Flip<{ pi: 'a' }>>>,
+  Expect<Equal<{ 3.14: 'pi'; true: 'bool' }, Flip<{ pi: 3.14; bool: true }>>>,
+  Expect<Equal<{ val2: 'prop2'; val: 'prop' }, Flip<{ prop: 'val'; prop2: 'val2' }>>>,
+]
+
