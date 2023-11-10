e431b7732f14d7f8fadf5ec1aa312b834e98a77f
WanderedToLa <rladntjr9808@gmail.com>
2023-11-10 11:05:02 +0900

    Update Trunc 5140

---
+++ 2_medium/Trunc.ts
@@ -0,0 +1,44 @@
+/*
+  5140 - Trunc
+  -------
+  by jiangshan (@jiangshanmeta) #medium #template-literal
+
+  ### Question
+
+  Implement the type version of ```Math.trunc```, which takes string or number and returns the integer part of a number by removing any fractional digits.
+
+  For example:
+
+  ```typescript
+  type A = Trunc<12.34> // 12
+  ```
+
+  > View on GitHub: https://tsch.js.org/5140
+*/
+
+import type { Equal, Expect } from "../utils";
+
+// type a = ".3";
+// type b<T> = T extends `${Dot}${any}` ? "0" : false;
+// type c = b<a>;
+//   `${T}` extends `${infer First}${Dot}${any}` ? First extends '' ? '0' : First : `${T}`
+
+type Dot = ".";
+type Trunc<T extends string | number> =
+  `${T}` extends `${infer First}${Dot}${any}`
+    ? First extends ""
+      ? "0"
+      : First
+    : `${T}`;
+
+type cases = [
+  Expect<Equal<Trunc<0.1>, "0">>,
+  Expect<Equal<Trunc<0.2>, "0">>,
+  Expect<Equal<Trunc<1.234>, "1">>,
+  Expect<Equal<Trunc<12.345>, "12">>,
+  Expect<Equal<Trunc<-5.1>, "-5">>,
+  Expect<Equal<Trunc<".3">, "0">>,
+  Expect<Equal<Trunc<"1.234">, "1">>,
+  Expect<Equal<Trunc<"-10.234">, "-10">>,
+  Expect<Equal<Trunc<10>, "10">>
+];
